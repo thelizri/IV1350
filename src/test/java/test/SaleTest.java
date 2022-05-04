36f4e4bb7057181e01f0f02b68a5e72fa557a12d
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-05-04 17:00:30 +0200

    Update

---
+++ src/test/java/test/SaleTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaleTest {
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void applyDiscount() {
+    }
+
+    @Test
+    void pay() {
+    }
+
+    @Test
+    void getRemainingAmountToPay() {
+    }
+
+    @Test
+    void endSale() {
+    }
+}
